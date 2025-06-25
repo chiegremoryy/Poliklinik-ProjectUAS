8eed946038a0c2ecd49d4ff1527b75e1d2aac089
chiegremoryy <chiescode@gmail.com>
2025-06-26 02:17:29 +0700

    donz (kali)

--- resources/views/pages/dokter/periksa.blade.php
+++ resources/views/pages/dokter/periksa.blade.php
@@ -26,7 +26,15 @@
 <div class="content-wrapper">
     <div class="content-header">
         <div class="container-fluid">
-            <h3>Periksa Pasien</h3>
+            <div class="row mb-2">
+                <div class="col-sm-6"></div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="#">Dokter</a></li>
+                        <li class="breadcrumb-item active">Periksa Pasien</li> 
+                    </ol>
+                </div>
+            </div>
         </div>
     </div>
 
